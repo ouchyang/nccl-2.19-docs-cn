75a0cf4a7db7bd4ff407ee45de340ac826d091b4
ouchyang <ouchyang@foxmail.com>
2025-08-26 11:03:41 +0800

    fist text cn

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,3 +61,9 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# multi-language docs
+language = 'en'
+locale_dirs = ['../locales/']   # path is example but recommended.
+gettext_compact = False  # optional.
+gettext_uuid = True  # optional.
